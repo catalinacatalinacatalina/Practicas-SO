73a64fb652aa98ddf3b6548f972ed28c795aa6b7
Catalina F. Mora <catalinafullanamora@gmail.com>
2025-06-08 15:41:52 +0200

    pr6 terminada - funciona

--- pr6/cocinero.c
+++ pr6/cocinero.c
@@ -1,12 +1,19 @@
+#define _POSIX_C_SOURCE 200809L //WSL
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <signal.h>
+
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include <bits/semaphore.h>
+#include <semaphore.h>
+#include <sys/types.h>
+
 
 #define M 10
 #define SHM_NAME "/caldero_shm"
@@ -14,63 +21,78 @@
 #define SEM_COCINAR "/sem_cocinar"
 #define SEM_LLENO "/sem_lleno"
 
-
 int finish = 0;
 int *raciones;
 int shm_fd;
 sem_t *mutex, *sem_cocinar, *sem_lleno;
 
-void putServingsInPot(int servings)
-{
+void limpiar() {
+    munmap(raciones, sizeof(int));
+    close(shm_fd);
+    shm_unlink(SHM_NAME);
+    sem_close(mutex);
+    sem_close(sem_cocinar);
+    sem_close(sem_lleno);
+    sem_unlink(SEM_MUTEX);
+    sem_unlink(SEM_COCINAR);
+    sem_unlink(SEM_LLENO);
 }
 
-void cook(void)
-{
-	while(!finish) {
-		putServingsInPot(M);
-	}
+void handler(int signo) {
+    finish = 1;
 }
 
-void handler(int signo)
-{
-	finish = 1;
+void putServingsInPot(int servings) {
+    // Espera a que le avisen para cocinar
+    sem_wait(sem_cocinar);
+    *raciones = servings;
+    printf("[COCINERO %d] Relleno el caldero con %d raciones\n", getpid(), servings);
+    // Despierta a los salvajes que esperan raciones
+    for(int i=0; i < servings; i++) {
+        sem_post(sem_lleno);
+    }
 }
 
-int main(int argc, char *argv[])
-{
-	int shm_fd;
+void cook(void) {
+    while(!finish) {
+        putServingsInPot(M);
+        usleep(100000); // Pequeño retardo para evitar busy wait
+    }
+}
 
-	if((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) < 0) {
-		perror("shm_open");
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char *argv[]) {
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
 
-	// Ajusta el tamaño del segmento de memoria
+    // Crear memoria compartida
+    if((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) < 0) {
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
     if (ftruncate(shm_fd, sizeof(int)) == -1) {
         perror("ftruncate");
         exit(EXIT_FAILURE);
     }
-
-
- 	// Mapear memoria en el espacio del proceso
-    int* raciones = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    raciones = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (raciones == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
     }
+    *raciones = 0;
 
-	*raciones = M;
-	printf("Caldero creado con %d raciones\n", *raciones);
+    // Crear semáforos (todos con valor inicial 0 menos el mutex que es 1)
+    mutex = sem_open(SEM_MUTEX, O_CREAT, 0666, 1);
+    sem_cocinar = sem_open(SEM_COCINAR, O_CREAT, 0666, 0);
+    sem_lleno = sem_open(SEM_LLENO, O_CREAT, 0666, 0);
 
-	getchar(); // Espera a que el usuario presione una tecla para continuar
+    printf("Caldero y semáforos creados.\nCtrl+C para limpiar y salir.\n");
 
-	//limpieza
-	if (munmap(raciones, sizeof(int)) == -1) {
-		perror("munmap");
-		exit(EXIT_FAILURE);
-	}
-	close(shm_fd);
-	shm_unlink(SHM_NAME);
+    cook();
 
-	return 0;
+    limpiar();
+    return 0;
 }
--- pr6/salvajes.c
+++ pr6/salvajes.c
@@ -2,21 +2,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <semaphore.h>
-#include <pthread.h>
-#include <asm-generic/fcntl.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <time.h>
 
 #define NUMITER 3
 #define SHM_NAME "/caldero_shm"
 #define SEM_MUTEX "/sem_mutex"
 #define SEM_COCINAR "/sem_cocinar"
 #define SEM_LLENO "/sem_lleno"
-#define NUMITER 3
 
 int* raciones;
+sem_t *mutex, *sem_cocinar, *sem_lleno;
 int getServingsFromPot(void) {
-	
+	int cogido = 0;
+	int avisado = 0;
+	while (!cogido) {
+		sem_wait(mutex); // entrar en sección crítica
+
+		if (*raciones == 0 && !avisado) {
+			printf("[SALVAJE %d] Caldero vacío, aviso al cocinero\n", getpid());
+			sem_post(sem_cocinar);  // avisar al cocinero que rellene
+			sem_wait(sem_lleno);    // esperar a que haya raciones
+			avisado = 1; // marcar que ya se ha avisado al cocinero
+		}
+
+		if (*raciones > 0) {
+			(*raciones)--;
+			printf("[SALVAJE %d] Coge ración, quedan %d\n", getpid(), *raciones);
+			sem_post(mutex); // salir de sección crítica
+			avisado = 0; // resetear aviso para el siguiente ciclo
+			cogido = 1; // ya he cogido mi ración
+		} else {
+			sem_post(mutex); // salir si no había raciones, para volver a intentar
+			// pequeño retardo para evitar busy wait muy agresivo
+			usleep(1000);
+		}
+	}
+	return 0;
 }
+
 
 void eat(void)
 {
@@ -27,11 +52,15 @@ void eat(void)
 
 void savages(void)
 {
-
+	for(int i=0; i<NUMITER; i++) {
+		getServingsFromPot();
+		eat();
+	}
 }
 
 int main(int argc, char *argv[])
 {
+	srand(time(NULL) ^ (getpid()<<16));
 	int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
     if (shm_fd == -1) {
         perror("shm_open");
@@ -45,27 +74,15 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    mutex = sem_open(SEM_MUTEX, 0);
+    sem_cocinar = sem_open(SEM_COCINAR, 0);
+    sem_lleno = sem_open(SEM_LLENO, 0);
 
-    printf("Salvaje: raciones en el caldero = %d\n", *raciones);
-
-    // Aquí iría la lógica de restar raciones y sincronización
-
-	sem_t *mutex = sem_open(SEM_MUTEX, 0);
-    sem_t *sem_cocinar = sem_open(SEM_COCINAR, 0);
-    sem_t *sem_lleno = sem_open(SEM_LLENO, 0);
-
-    pid_t child = getpid();
-
-	for(int i=0; i<NUMITER; i++) {
-		//
-		getServingsFromPot();
-		eat();
-		//
-	}
+    savages();
 
     // Limpieza
     munmap(raciones, sizeof(int));
     close(shm_fd);
-	
+
 	return 0;
 }
